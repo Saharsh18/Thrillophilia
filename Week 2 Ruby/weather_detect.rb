e31f6acf356a96dbe60d643f8a38aa782384b11b
Saharsh Sadabhartia <51753172+Saharsh18@users.noreply.github.com>
2022-01-08 15:11:50 +0530

    Add files via upload

--- Week 2 Ruby/weather_detect.rb
+++ Week 2 Ruby/weather_detect.rb
@@ -1,1 +1,25 @@
+require 'net/http'
+require 'json'
 
+
+def weather_detect(city)
+
+    params = {
+        :access_key => "1eca0554caf0f8a79c542f3d706481fc",  #API Key
+        :query => city 
+    }
+
+    uri = URI('http://api.weatherstack.com/current')
+    uri.query = URI.encode_www_form(params)
+    json = Net::HTTP.get(uri)
+    api_response = JSON.parse(json)
+    puts "The current temperature in #{api_response['location']['name']}(#{api_response['location']['country']}) is #{api_response['current']['temperature']}°C"
+
+end
+
+#Taking the input from the user
+puts "Enter a city name to see the current temperature"
+city = gets.chomp
+
+#Function Calling 
+weather_detect(city) 
